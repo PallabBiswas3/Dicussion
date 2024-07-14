50405c0dda3a7570510b3b73cad50f49f80cc9e3
PallabBiswas3 <pallabbiswas891@gmail.com>
2024-07-14 17:20:16 +0530

    Updating size of cards and color

--- blogcard/src/App.js
+++ blogcard/src/App.js
@@ -30,6 +30,19 @@ const blogData = [
     "description": "Phasellus faucibus facilisis sapien, sed interdum sapien placerat in. Aenean magna sapien, condimentum ut mattis ac, congue tempor eros. Integer vel sem a diam pretium pulvinar. Quisque diam erat, consequat at nunc nec, luctus hendrerit ex. Maecenas egestas libero felis, ac interdum ante gravida vitae. Donec efficitur lacus placerat, porttitor enim eu, mattis turpis. Etiam sed mollis nunc.",
     "imageUrl": "https://storage.googleapis.com/chydlx/codepen/blog-cards/image-2.jpg",
     "alt": true
+  },
+  {
+    "title": "Learning to Code",
+    "subtitle": "Opening a door to the future",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac mattis mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin semper nisi, vestibulum egestas mauris malesuada ac. Quisque ac aliquam mauris, at porttitor neque. Sed sit amet laoreet mauris. Aliquam laoreet interdum ante vitae rutrum.",
+    "imageUrl": "https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg"
+  },
+  {
+    "title": "Mastering the Language",
+    "subtitle": "Java is not the same as JavaScript",
+    "description": "Phasellus faucibus facilisis sapien, sed interdum sapien placerat in. Aenean magna sapien, condimentum ut mattis ac, congue tempor eros. Integer vel sem a diam pretium pulvinar. Quisque diam erat, consequat at nunc nec, luctus hendrerit ex. Maecenas egestas libero felis, ac interdum ante gravida vitae. Donec efficitur lacus placerat, porttitor enim eu, mattis turpis. Etiam sed mollis nunc.",
+    "imageUrl": "https://storage.googleapis.com/chydlx/codepen/blog-cards/image-2.jpg",
+    "alt": true
   }
 ];
 
